2ad2851b9cc483127e21b7fc16f1ed289fb0c681
Tianhong Dai <tianhong.dai15@imperial.ac.uk>
2021-11-17 15:42:44 +0000

    update

--- sobel.py
+++ sobel.py
@@ -8,46 +8,46 @@ this is the script to calculate the sobel operator
 """
 
 def sobel_estimator(img, cuda=False, save_figs=False):
-	"""
-	sobel descriptor
-	"""
-	sobel_weight_x = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
-	sobel_weight_y = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
-	sobel_weight_45 = np.array([[-2, -1, 0], [-1, 0, 1], [0, 1, 2]])
-	sobel_weight_135 = np.array([[0, -1, -2], [1, 0, -1], [2, 1, 0]])
-	# process sobel weights
-	sobel_weight_x = torch.tensor(sobel_weight_x, dtype=torch.float32, device='cuda' if cuda else 'cpu', requires_grad=False).unsqueeze(0).unsqueeze(0)
-	sobel_weight_y = torch.tensor(sobel_weight_y, dtype=torch.float32, device='cuda' if cuda else 'cpu', requires_grad=False).unsqueeze(0).unsqueeze(0)
-	sobel_weight_45 = torch.tensor(sobel_weight_45, dtype=torch.float32, device='cuda' if cuda else 'cpu', requires_grad=False).unsqueeze(0).unsqueeze(0)
-	sobel_weight_135 = torch.tensor(sobel_weight_135, dtype=torch.float32, device='cuda' if cuda else 'cpu', requires_grad=False).unsqueeze(0).unsqueeze(0)
-	# process
-	channels = img.size()[1]
-	sobel_weight_x = sobel_weight_x.repeat(channels, 1, 1, 1)
-	sobel_weight_y = sobel_weight_y.repeat(channels, 1, 1, 1)
-	sobel_weight_45 = sobel_weight_45.repeat(channels, 1, 1, 1)
-	sobel_weight_135 = sobel_weight_135.repeat(channels, 1, 1, 1)
-	# get edge
-	edge_x = F.conv2d(img, sobel_weight_x, padding=1, stride=1, groups=channels, bias=None)
-	edge_y = F.conv2d(img, sobel_weight_y, padding=1, stride=1, groups=channels, bias=None)
-	edge_45 = F.conv2d(img, sobel_weight_45, padding=1, stride=1, groups=channels, bias=None)
-	edge_135 = F.conv2d(img, sobel_weight_135, padding=1, stride=1, groups=channels, bias=None)
-	if save_figs:
-		img_x = edge_x.detach().cpu().numpy()
-		img_y = edge_y.detach().cpu().numpy()
-		if channels > 1:
-			img_x = img_x[0, 0, :, :]
-			img_y = img_y[0, 0, :, :]
-		else:
-			img_x = img_x.squeeze()
-			img_y = img_y.squeeze()
-		img_x = np.clip(img_x, 0, 1)
-		img_y = np.clip(img_y, 0, 1)
-		cv2.imwrite('edge_x.png', (img_x * 255).astype(np.uint8))
-		cv2.imwrite('edge_y.png', (img_y * 255).astype(np.uint8))
-	return edge_x, edge_y, edge_45, edge_135
+    """
+    sobel descriptor
+    """
+    sobel_weight_x = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
+    sobel_weight_y = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+    sobel_weight_45 = np.array([[-2, -1, 0], [-1, 0, 1], [0, 1, 2]])
+    sobel_weight_135 = np.array([[0, -1, -2], [1, 0, -1], [2, 1, 0]])
+    # process sobel weights
+    sobel_weight_x = torch.tensor(sobel_weight_x, dtype=torch.float32, device='cuda' if cuda else 'cpu', requires_grad=False).unsqueeze(0).unsqueeze(0)
+    sobel_weight_y = torch.tensor(sobel_weight_y, dtype=torch.float32, device='cuda' if cuda else 'cpu', requires_grad=False).unsqueeze(0).unsqueeze(0)
+    sobel_weight_45 = torch.tensor(sobel_weight_45, dtype=torch.float32, device='cuda' if cuda else 'cpu', requires_grad=False).unsqueeze(0).unsqueeze(0)
+    sobel_weight_135 = torch.tensor(sobel_weight_135, dtype=torch.float32, device='cuda' if cuda else 'cpu', requires_grad=False).unsqueeze(0).unsqueeze(0)
+    # process
+    channels = img.size()[1]
+    sobel_weight_x = sobel_weight_x.repeat(channels, 1, 1, 1)
+    sobel_weight_y = sobel_weight_y.repeat(channels, 1, 1, 1)
+    sobel_weight_45 = sobel_weight_45.repeat(channels, 1, 1, 1)
+    sobel_weight_135 = sobel_weight_135.repeat(channels, 1, 1, 1)
+    # get edge
+    edge_x = F.conv2d(img, sobel_weight_x, padding=1, stride=1, groups=channels, bias=None)
+    edge_y = F.conv2d(img, sobel_weight_y, padding=1, stride=1, groups=channels, bias=None)
+    edge_45 = F.conv2d(img, sobel_weight_45, padding=1, stride=1, groups=channels, bias=None)
+    edge_135 = F.conv2d(img, sobel_weight_135, padding=1, stride=1, groups=channels, bias=None)
+    if save_figs:
+        img_x = edge_x.detach().cpu().numpy()
+        img_y = edge_y.detach().cpu().numpy()
+        if channels > 1:
+            img_x = img_x[0, 0, :, :]
+            img_y = img_y[0, 0, :, :]
+        else:
+            img_x = img_x.squeeze()
+            img_y = img_y.squeeze()
+        img_x = np.clip(img_x, 0, 1)
+        img_y = np.clip(img_y, 0, 1)
+        cv2.imwrite('edge_x.png', (img_x * 255).astype(np.uint8))
+        cv2.imwrite('edge_y.png', (img_y * 255).astype(np.uint8))
+    return edge_x, edge_y, edge_45, edge_135
 
 if __name__ == '__main__':
-	img = cv2.imread('img.jpeg',).astype(np.float32) / 255.0
-	img = np.transpose(img, (2, 0, 1))
-	img = torch.tensor(img, dtype=torch.float32, device='cuda').unsqueeze(0)
-	edge_x, edge_y, _, _ = sobel_estimator(img, cuda=True, save_figs=True)
+    img = cv2.imread('img.jpeg',).astype(np.float32) / 255.0
+    img = np.transpose(img, (2, 0, 1))
+    img = torch.tensor(img, dtype=torch.float32, device='cuda').unsqueeze(0)
+    edge_x, edge_y, _, _ = sobel_estimator(img, cuda=True, save_figs=True)
--- train.py
+++ train.py
@@ -29,71 +29,71 @@ parser.add_argument('--save-path', type=str, default='saved_models')
 
 
 def mu_tonemap(x, mu=5000):
-	return torch.log(1 + mu * x) / np.log(1 + mu)
+    return torch.log(1 + mu * x) / np.log(1 + mu)
 
 def process_tensors(in_ldrs, in_hdrs, ref_hdr, use_cuda):
-	"""
-	convert the tensor
-	"""
-	# process the input
-	im1 = torch.cat([in_ldrs[:, :, :, :3], in_hdrs[:, :, :, :3]], dim=3).permute(0, 3, 1, 2)
-	im2 = torch.cat([in_ldrs[:, :, :, 3:6], in_hdrs[:, :, :, 3:6]], dim=3).permute(0, 3, 1, 2)
-	im3 = torch.cat([in_ldrs[:, :, :, 6:9], in_hdrs[:, :, :, 6:9]], dim=3).permute(0, 3, 1, 2)
-	# process the ground truth
-	ref_hdr = ref_hdr.permute(0, 3, 1, 2)
-	# send the tensor into the cuda
-	im1 = im1.to('cuda' if use_cuda else 'cpu')
-	im2 = im2.to('cuda' if use_cuda else 'cpu')
-	im3 = im3.to('cuda' if use_cuda else 'cpu')
-	ref_hdr = ref_hdr.to('cuda' if use_cuda else 'cpu')
-	return im1, im2, im3, ref_hdr
+    """
+    convert the tensor
+    """
+    # process the input
+    im1 = torch.cat([in_ldrs[:, :, :, :3], in_hdrs[:, :, :, :3]], dim=3).permute(0, 3, 1, 2)
+    im2 = torch.cat([in_ldrs[:, :, :, 3:6], in_hdrs[:, :, :, 3:6]], dim=3).permute(0, 3, 1, 2)
+    im3 = torch.cat([in_ldrs[:, :, :, 6:9], in_hdrs[:, :, :, 6:9]], dim=3).permute(0, 3, 1, 2)
+    # process the ground truth
+    ref_hdr = ref_hdr.permute(0, 3, 1, 2)
+    # send the tensor into the cuda
+    im1 = im1.to('cuda' if use_cuda else 'cpu')
+    im2 = im2.to('cuda' if use_cuda else 'cpu')
+    im3 = im3.to('cuda' if use_cuda else 'cpu')
+    ref_hdr = ref_hdr.to('cuda' if use_cuda else 'cpu')
+    return im1, im2, im3, ref_hdr
 
 if __name__ == '__main__':
-	# get the arguments
-	args = parser.parse_args()
-	# creat the save dir
-	if not os.path.exists(args.save_path):
-		os.makedirs(args.save_path, exist_ok=True)
-	# get the dataset
-	dataset = DatasetLoader(args.dataset_path)
-	train_loader = DataLoader(dataset=dataset, batch_size=args.batch_size, shuffle=True, num_workers=0)
-	# define the network
-	net = Wavelet_UNet(args)
-	if args.use_parallel:
-		net = nn.DataParallel(net)
-	# if use cuda
-	if args.cuda:
-		net.cuda()
-	# define the optimizer
-	optim = torch.optim.Adam(net.parameters(), lr=args.lr)
-	# start the training
-	net.train()
-	best_psnr_mu = 0
-	for epoch_id in range(args.epochs):
-		for i, (in_ldrs, in_hdrs, ref_hdr) in enumerate(train_loader):
-			im1, im2, im3, ref_hdr = process_tensors(in_ldrs, in_hdrs, ref_hdr, use_cuda=args.cuda)
-			# tonemapping and calculate the loss
-			generate_hdr = net(im1, im2, im3)
-			# tone mapping
-			generate_hdr_tonemap = mu_tonemap(generate_hdr)
-			ref_hdr_tonemap = mu_tonemap(ref_hdr)
-			loss = torch.abs(generate_hdr_tonemap - ref_hdr_tonemap).mean()
-			# get the sobel loss - currently we use the normal sobel
-			generate_grad_x, generate_grad_y, generate_grad_45, generate_grad_135 = sobel_estimator(generate_hdr_tonemap, cuda=args.cuda)
-			ref_grad_x, ref_grad_y, ref_grad_45, ref_grad_135 = sobel_estimator(ref_hdr_tonemap, cuda=args.cuda)
-			sobel_loss = 0.25 * torch.abs(generate_grad_x - ref_grad_x).mean() + 0.25 * torch.abs(generate_grad_y - ref_grad_y).mean()
-			# the total loss
-			loss = loss + sobel_loss
-			# start to update the network
-			optim.zero_grad()
-			loss.backward()
-			optim.step()
-		print('[{}] epoch: {}, iteration: {}/{}, loss:{:.3f}, best_psnr: {:.3f}'.format(datetime.now(), epoch_id, i+1, len(train_loader), loss.item(), best_psnr_mu))
-		if epoch_id % 10 == 0:
-			# eval the model
-			print('[{}] start to evaluate the model'.format(datetime.now()))
-			net.eval()
-			cur_psnr_mu = eval_network(args, net)
-			if cur_psnr_mu > best_psnr_mu:
-				best_psnr_mu = cur_psnr_mu
-				torch.save([net.state_dict(), optim.state_dict()], '{}/model.pt'.format(args.save_path))
+    # get the arguments
+    args = parser.parse_args()
+    # creat the save dir
+    if not os.path.exists(args.save_path):
+        os.makedirs(args.save_path, exist_ok=True)
+    # get the dataset
+    dataset = DatasetLoader(args.dataset_path)
+    train_loader = DataLoader(dataset=dataset, batch_size=args.batch_size, shuffle=True, num_workers=0)
+    # define the network
+    net = Wavelet_UNet(args)
+    if args.use_parallel:
+        net = nn.DataParallel(net)
+    # if use cuda
+    if args.cuda:
+        net.cuda()
+    # define the optimizer
+    optim = torch.optim.Adam(net.parameters(), lr=args.lr)
+    # start the training
+    net.train()
+    best_psnr_mu = 0
+    for epoch_id in range(args.epochs):
+        for i, (in_ldrs, in_hdrs, ref_hdr) in enumerate(train_loader):
+            im1, im2, im3, ref_hdr = process_tensors(in_ldrs, in_hdrs, ref_hdr, use_cuda=args.cuda)
+            # tonemapping and calculate the loss
+            generate_hdr = net(im1, im2, im3)
+            # tone mapping
+            generate_hdr_tonemap = mu_tonemap(generate_hdr)
+            ref_hdr_tonemap = mu_tonemap(ref_hdr)
+            loss = torch.abs(generate_hdr_tonemap - ref_hdr_tonemap).mean()
+            # get the sobel loss - currently we use the normal sobel
+            generate_grad_x, generate_grad_y, generate_grad_45, generate_grad_135 = sobel_estimator(generate_hdr_tonemap, cuda=args.cuda)
+            ref_grad_x, ref_grad_y, ref_grad_45, ref_grad_135 = sobel_estimator(ref_hdr_tonemap, cuda=args.cuda)
+            sobel_loss = 0.25 * torch.abs(generate_grad_x - ref_grad_x).mean() + 0.25 * torch.abs(generate_grad_y - ref_grad_y).mean()
+            # the total loss
+            loss = loss + sobel_loss
+            # start to update the network
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+        print('[{}] epoch: {}, iteration: {}/{}, loss:{:.3f}, best_psnr: {:.3f}'.format(datetime.now(), epoch_id, i+1, len(train_loader), loss.item(), best_psnr_mu))
+        if epoch_id % 10 == 0:
+            # eval the model
+            print('[{}] start to evaluate the model'.format(datetime.now()))
+            net.eval()
+            cur_psnr_mu = eval_network(args, net)
+            if cur_psnr_mu > best_psnr_mu:
+                best_psnr_mu = cur_psnr_mu
+                torch.save([net.state_dict(), optim.state_dict()], '{}/model.pt'.format(args.save_path))
